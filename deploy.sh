f90a2544cdda5c8d640fea167c58057b0f57fbf6
kiwipeach <1099501218@qq.com>
2019-08-04 23:58:27 +0800

    部署脚本更新

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 #jenkins打包成war包之后存放的目录
-SOURCE_CODE_PATH="/webapp/nice-blog-sys"
+SOURCE_CODE_PATH="/webapp/nice-blog-sys/blog-sys-web"
 TARGET_JAR_PATH=$SOURCE_CODE_PATH/target/NiceBlog.jar
 WEB_LOGFILE=/weblogs/nice-blog-sys/nice-blog-sys.out
 
 #需要的环境变量：SOURCE_CODE_PATH(项目源代码路径)
 # 1) 拉取gitee代码
-echo "[step 1:]  pull source code from gitee."
+echo "[step 1:]  从gitee上拉取代码"
 cd $SOURCE_CODE_PATH
 git pull origin master
 
 # 2) mvn编译打包
-echo "[step 2:]  clean compile package code with maven."
+echo "[step 2:]  用maven进行编译打包"
 mvn clean compile package
 
 # 3) springboot服务重启
 
-echo "[step 3:]  服务重启"
+echo "[step 3:]  重启springboot进城服务"
 blog_sys_pid=`ps -ef | grep java| grep $TARGET_JAR_PATH |awk '{print $2}'`
 
 if [ -n "$blog_sys_pid" ]
